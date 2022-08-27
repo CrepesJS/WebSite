73edf2d8debb8a6437e285bb37c9a90326b91124
Nikola Hristov <nikola@playform.cloud>
2022-08-27 19:10:02 +0300

    Cleanup

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,18 @@
 import type { AstroUserConfig } from "astro";
-
-import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 import critters from "astro-critters";
 
-export default {
-	site: "https://crepes.dev",
-	integrations: [
-		sitemap(),
-		critters(),
-		compress({
-			logger: 1,
-		}),
-	],
-} as AstroUserConfig;
+import sitemap from "@astrojs/sitemap";
+
+export default (): AstroUserConfig => {
+	return {
+		site: "https://crepes.dev",
+		integrations: [
+			sitemap(),
+			critters(),
+			compress({
+				logger: 1,
+			}),
+		],
+	};
+};
