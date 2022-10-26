5a59963b85c3dec0e478783087e3c29145ecdad1
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-27 01:17:23 +0300

    squash!

---
+++ src/scripts/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyCCtwe3PjtMkftqCgc0GC2k3cbKb9vYdGk",
+	appId: "1:451350706276:web:b8dcaa0d46ce2aca058771",
+	authDomain: "crepesjs-com.firebaseapp.com",
+	databaseURL: "https://crepesjs-com.firebaseio.com",
+	measurementId: "G-M0G8VCVKY4",
+	messagingSenderId: "451350706276",
+	projectId: "crepesjs-com",
+	storageBucket: "crepesjs-com.appspot.com",
+};
+
+initializeApp(firebaseConfig);
