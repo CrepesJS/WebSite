b721533a99f150c85c20a2fac4e9ce09878936b3
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-24 04:05:02 +0200

    Prettifies everything

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
 	apiKey: "AIzaSyCCtwe3PjtMkftqCgc0GC2k3cbKb9vYdGk",
 	authDomain: "crepesjs-com.firebaseapp.com",
-  	databaseURL: "https://crepesjs-com.firebaseio.com",
+	databaseURL: "https://crepesjs-com.firebaseio.com",
 	projectId: "crepesjs-com",
 	storageBucket: "crepesjs-com.appspot.com",
 	messagingSenderId: "451350706276",
 	appId: "1:451350706276:web:b8dcaa0d46ce2aca058771",
-	measurementId: "G-9E2P7HPRNM"
+	measurementId: "G-9E2P7HPRNM",
 };
 
 initializeApp(firebaseConfig);
