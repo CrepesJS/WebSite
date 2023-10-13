d98ad2572a52cf6ceaac5e8e3c32aad1fdc8e513
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-13 13:09:22 +0300

    squash!

--- Source/Script/Firebase.ts
+++ Source/Script/Firebase.ts
@@ -1,4 +1,6 @@
-(await import("firebase/app")).initializeApp({
+import { initializeApp } from "firebase/app";
+
+initializeApp({
 	apiKey: "AIzaSyCCtwe3PjtMkftqCgc0GC2k3cbKb9vYdGk",
 	authDomain: "crepesjs-com.firebaseapp.com",
 	databaseURL: "https://crepesjs-com.firebaseio.com",
@@ -8,4 +10,3 @@
 	appId: "1:451350706276:web:b8dcaa0d46ce2aca058771",
 	measurementId: "G-9E2P7HPRNM",
 });
-
