aa3d353944a6325b10dfa6254bf874830efc6a9d
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-14 06:28:13 +0300

    squash!

--- Source/Script/Firebase.ts
+++ Source/Script/Firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 
 initializeApp({
-<<<<<<< HEAD
 	apiKey: "AIzaSyCCtwe3PjtMkftqCgc0GC2k3cbKb9vYdGk",
 	authDomain: "crepesjs-com.firebaseapp.com",
 	databaseURL: "https://crepesjs-com.firebaseio.com",
@@ -10,14 +9,4 @@ initializeApp({
 	messagingSenderId: "451350706276",
 	appId: "1:451350706276:web:b8dcaa0d46ce2aca058771",
 	measurementId: "G-9E2P7HPRNM",
-=======
-	apiKey: "", // TODO Replace with your API key
-	appId: "", // TODO Replace with your app ID
-	authDomain: "", // TODO Replace with your auth domain
-	databaseURL: "", // TODO Replace with your database URL
-	measurementId: "", // TODO Replace with your measurement ID
-	messagingSenderId: "", // TODO Replace with your messaging sender ID
-	projectId: "", // TODO Replace with your project ID
-	storageBucket: "", // TODO Replace with your storage bucket
->>>>>>> upstream/main
 });
